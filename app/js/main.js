d3ceadffa466d34a44bd8eebef3364be0f5cde87
Kerthin <belichenko.roden@list.ru>
2018-09-19 17:51:14 +0300

    The connection and initialization of the slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,3 +2,13 @@
 	JQuery.min.js
 ********************/
 //= ../../bower_components/jquery/dist/jquery.min.js
+
+/********************
+	swiper.min.js
+********************/
+//= partials/libs/swiper.min.js
+
+/********************
+	swiperInitializations.js
+********************/
+//= partials/swiperInitializations.js
---
+++ app/js/partials/swiperInitializations.js
@@ -0,0 +1,4 @@
+var swiper = new Swiper('.swiper-container', {
+	noSwipingClass: 'falseSwiping',
+	noSwiping: true
+});
